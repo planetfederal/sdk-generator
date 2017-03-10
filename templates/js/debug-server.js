201eb149bdc33cd72506105dd9891bd7ea88594c
bartvde <bartvde@boundlessgeo.com>
2017-03-10 11:11:01 +0100

    Allow specifying the port of the debug server (SDK-357)

--- templates/js/debug-server.js
+++ templates/js/debug-server.js
@@ -1,1 +1,9 @@
-require('boundless-sdk-tools').startServer();
+var tools = require('boundless-sdk-tools');
+var port;
+process.argv.slice(2).forEach(function (arg) {
+  var flag = arg.split('=')[0];
+  if (flag == '--port') {
+    port = arg.split('=')[1];
+  }
+});
+tools.startServer(undefined, port);
